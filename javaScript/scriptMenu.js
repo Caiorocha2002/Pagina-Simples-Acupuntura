2309aec78d01f6169cc2ceee063b32c676c6b5f1
Caio Rocha <caiorochauba@outlook.com>
2022-05-01 00:26:50 -0300

    add projetos

---
+++ javaScript/scriptMenu.js
@@ -0,0 +1,8 @@
+function menu() {
+    const listaMenu = document.getElementById('listaMenu');
+    const menu = document.getElementById('menu');
+    listaMenu.classList.toggle("on");
+    menu.classList.toggle("checado")
+
+
+};
